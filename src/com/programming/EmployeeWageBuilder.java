69d7c653d8aeffba39ab3d27b388361c0454404b
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 12:17:12 +0530

    Added:Employee attendance check

--- src/com/programming/EmployeeWageBuilder.java
+++ src/com/programming/EmployeeWageBuilder.java
@@ -1,8 +1,23 @@
 package com.programming;
 
 public class EmployeeWageBuilder {
+    public static final int isPresent = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
+
+        int employeeCheck = (int) Math.floor((Math.random() * 10) % 3);
+            /*switch case statement is used  for
+            checking his presence to duty*/
+        switch (employeeCheck) {
+
+            case isPresent:
+                System.out.println("Employee is present");;
+                break;
+
+            default:
+                System.out.println("Employee is absent");;
+                break;
+        }
     }
 }
