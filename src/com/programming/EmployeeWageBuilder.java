d099ed40e6b0314af8b4bec35c0756ddccde1e68
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 12:11:54 +0530

    Added:Welcome to employee wage computation

---
+++ src/com/programming/EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+package com.programming;
+
+public class EmployeeWageBuilder {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation");
+    }
+}
