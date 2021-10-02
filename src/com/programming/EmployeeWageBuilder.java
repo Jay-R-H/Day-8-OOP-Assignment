bcaf30c8c5a97353d49c911567d076f981c49790
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 12:51:04 +0530

    Added:Employee daily wage computation

--- src/com/programming/EmployeeWageBuilder.java
+++ src/com/programming/EmployeeWageBuilder.java
@@ -2,21 +2,25 @@ package com.programming;
 
 public class EmployeeWageBuilder {
     public static final int isPresent = 1;
+    static final int wagePerHour = 20;
+    static final int fullDayHour = 8;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
 
-        int employeeCheck = (int) Math.floor((Math.random() * 10) % 3);
+        int employeeCheck = (int) Math.floor((Math.random() * 10) % 2);
             /*switch case statement is used  for
             checking his presence to duty*/
         switch (employeeCheck) {
 
             case isPresent:
-                System.out.println("Employee is present");;
+                System.out.println("Employee is present");
+                int wage = (wagePerHour * fullDayHour);
+                System.out.println("Employee has earned " + wage );
                 break;
 
             default:
-                System.out.println("Employee is absent");;
+                System.out.println("Employee is absent");
                 break;
         }
     }
