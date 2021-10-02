6f6915601f44067dcc7f9cbb9c5fc6cf2154a733
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 13:11:44 +0530

    Added:Wage calculation for a month

--- src/com/programming/EmployeeWageBuilder.java
+++ src/com/programming/EmployeeWageBuilder.java
@@ -4,6 +4,8 @@ public class EmployeeWageBuilder {
     public static final int isFullTime = 1;
     public static final int isPartTime = 2;
     static final int wagePerHour = 20;
+    static final int totalWorkingDays = 20;
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
@@ -12,23 +14,29 @@ public class EmployeeWageBuilder {
             /*switch case statement is used  for
             checking his presence to duty*/
         int workingHrs = 0;
-        switch (employeeCheck) {
+        int workingDays = 0;
+        int totalWage=0;
+        while (totalWorkingDays > workingDays) {
+            switch (employeeCheck) {
 
-            case isFullTime:
+                case isFullTime:
 
-                workingHrs = 8;
-                break;
+                    workingHrs = 8;
+                    break;
 
-            case isPartTime:
-                workingHrs = 4;
-                break;
+                case isPartTime:
+                    workingHrs = 4;
+                    break;
 
-            default:
-                System.out.println("Employee is absent");
-                workingHrs = 0;
-                break;
+                default:
+                    System.out.println("Employee is absent");
+                    workingHrs = 0;
+                    break;
+            }
+            workingDays++;
+            int wage = (wagePerHour * workingHrs);
+            totalWage+=wage;
+            System.out.println("The employee has worked for " + workingHrs + " hrs ,and earned " + totalWage);
         }
-        int wage = (wagePerHour * workingHrs);
-        System.out.println("The employee has worked for " + workingHrs + " hrs ,and earned " + wage);
     }
 }
